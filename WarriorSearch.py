a72fd07b7710189a20bd80097b18f37cd83a2ccc
GarrisonTylerSmith <gts35@nau.edu>
2018-02-18 19:54:42 -0700

    Add WarriorSearch, establish basic functions

---
+++ WarriorSearch.py
@@ -0,0 +1,24 @@
+from Graphs import GraphViz
+from .SearchNode import SearchNode
+
+
+class WarriorSearch:
+
+	graph = []
+	open_list = []
+	start_node = None
+	end_node = None
+
+	def show_open_list():
+
+		print("Open List: ", open_list)
+
+	def generate_successor(startNode):
+
+
+
+	def insert_into_open(front, end, openList):
+
+
+
+
