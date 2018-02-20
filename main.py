d6bd5dfa8a937fd7f6b39f62527e29c2aece8298
GarrisonTylerSmith <gts35@nau.edu>
2018-02-19 21:17:53 -0700

    Update main for test purposes

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-# inital comment
+# Imports
+from search import WarriorSearch
+from search import SearchNode
+
+# Test GraphSearch
+
+# Initialize graph search
+print("Loading graph file")
+gs = WarriorSearch("test_data/30.txt")
+print("Graph loaded:", gs.graph)
+
+# Set goals
+gs.set_start("U")
+gs.set_goal("T")
+gs.show_open()
+
+# Generate successors
+successors = gs.generate_successors(gs.open_list[0])
+print("'U' Successors:", successors)
+
+# Insert successors and re-show open
+gs.insert_nodes(successors, "order")
+gs.show_open()
+
+# Test duplicates
+gs.insert_nodes([SearchNode("K", 500),
+                 SearchNode("C", 19),
+                 SearchNode("J", 10)])
+gs.show_open()
